5a462712d8695f0e9eaff35d0e64a6b5a4e56b72
Adeoluwa Apata <40585277+Adeoluwa0@users.noreply.github.com>
2021-07-23 18:00:47 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ module.exports = {
     images:{
     domains:["links.papareact.com","image.tmdb.org"],
 },
+    eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+    
     
 };
